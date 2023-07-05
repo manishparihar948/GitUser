0069284c2fab3712fa44491a175126e89cd65e13
manishparihar948 <manish.parihar948@gmail.com>
2023-07-05 22:35:18 +0200

    Initial filde added

---
+++ GitUserChannel/GitUserChannelApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitUserChannelApp.swift
+//  GitUserChannel
+//
+//  Created by Manish Parihar on 05.07.23.
+//
+
+import SwiftUI
+
+@main
+struct GitUserChannelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
